b2cff3e29282fc2e759ea673ff58d49787d6ad65
Heavy <Heavy4@mail.ru>
2022-12-14 17:01:14 +0300

    23112

---
+++ task47/task47.cs
@@ -0,0 +1,23 @@
+﻿// Задача 47. Задайте двумерный массив (вручную) размером m×n, заполненный вещественными числами.
+// m = 3, n = 4.    0,5 7 -2 -0,2    1 -3,3 8 -9,9    8 7,8 -7,1 9
+
+Console.WriteLine("введите размер массива m и n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,]array2d = new int[m,n];
+string [] array = new string [n];
+Console.WriteLine("введите элементы массива строкой через запятую.");
+for(int i = 0; i<m; i++)
+{
+    string text = Console.ReadLine();
+    string[] array[m,n] = new.Split(",");
+    for(int j=0; j<n; j++)
+    {
+            array2d[i,j] = Convert.ToInt32(array[j]);
+            Console.Write(array2d[i,j]);
+    }
+    Console.WriteLine();
+}
+
+
+
---
+++ task50/task50.cs
@@ -0,0 +1,27 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает элемент в двумерном массиве, и возвращает индекс 
+//этого элемента или же указание, что такого элемента нет. Если элементов несколько, то выводим позицию каждого.
+//Двумерный массив задаётся на ваш выбор.
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4           17 -> такого числа в массиве нет
+Console.WriteLine("Ведите проверяемое число n");
+int n = Convert.ToInt32(Console.ReadLine());
+int m=4,k=4;
+int [,] array2d = new int[m, k];
+int z=0;
+Random random = new Random();
+for (int i=0; i<array2d.GetLength(0);i++)
+{
+    for (int j=0; j<array2d.GetLength(1); j++)
+    {
+        array2d[i,j]=random.Next(0,10);
+        Console.Write(array2d[i,j]+" ");
+        if (array2d[i,j]==n)
+            Console.Write("Индекс элемента: "+i+","+j+" ");
+        else
+            z++;
+    }
+Console.WriteLine();
+}
+if (z>=m*k)
+Console.Write("Такого числа в массиве нет");
